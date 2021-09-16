60c499030525d22ae97a33f1cd405bec6bc4a603
Marcel Henrique <60407893+marcel1712@users.noreply.github.com>
2021-09-16 23:17:07 +0000

    Initial commit

---
+++ musicas.js
@@ -0,0 +1,19 @@
+function sound1(){
+  var music = new Audio('eletric-love.mp3')
+  music.play()
+}
+
+function sound2(){
+  var music = new Audio('happier-than-ever.mp3')
+  music.play()
+}
+
+function sound3(){
+  var music = new Audio('ykwim.mp3')
+  music.play()
+}
+
+function sound4(){
+  var music = new Audio('come a little closer.mp3')
+  music.play()
+}
